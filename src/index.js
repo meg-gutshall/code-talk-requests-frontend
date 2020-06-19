f9024b81cc03b6836e5f80fe14d83afe136db358
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-18 22:52:32 -0400

    Create get fetch for requests

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-document.addEventListener('DOMContentLoaded', e => console.log('loaded'));
+const REQS_URL = "http://localhost:3000/api/v1/requests"
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("loaded")
+  fetchRequests()
+});
+
+function fetchRequests() {
+  fetch(REQS_URL)
+    .then(resp => resp.json())
+    .then(reqs => console.log(reqs))
+}
