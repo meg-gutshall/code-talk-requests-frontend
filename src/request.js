6615e188c9a34dff4cc1515c8cbbe3df1378cad5
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-24 21:18:49 -0400

    Add a renderRequest function

--- src/request.js
+++ src/request.js
@@ -6,6 +6,18 @@ class Request {
     this.student_id = requestData.student_id;
   }
 
+  renderRequest(request) {
+    return `
+      <div class="card-body">
+        <h3 class="card-title">${this.topic}</h3>
+        <h4 class="card-subtitle mb-2 text-muted">${this.module}</h4>
+        <p class="card-text">${this.description}</p>
+      </div>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    `
+  }
 
 }
 
