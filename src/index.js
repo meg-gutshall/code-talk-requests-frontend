ddf6b63fcc0ce35d1ec3c60355f6f42aaef83dad
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-25 07:52:49 -0400

    Implement webpack -- Getting Started guide

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import bootstrap from 'bootstrap';
+import _ from 'lodash';
 
-const REQS_URL = "http://localhost:3000/api/v1/requests"
-const MODULES = ["Git & GitHub", "Intro to Ruby", "HTML & CSS", "Procedural Ruby", "Object-Oriented Ruby", "SQL", "ORMs & ActiveRecord", "Rack", "Sinatra", "Rails", "JavaScript", "React", "Redux", "Other"]
+function component() {
+  const element = document.createElement('div');
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("loaded")
-  fetchRequests()
-});
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-function fetchRequests() {
-  fetch(REQS_URL)
-    .then(resp => resp.json())
-    .then(requests => console.log("fetchRequests -> requests", requests))
+  return element;
 }
+
+document.body.appendChild(component());
+
+// PROJECT CODE BELOW -- DO NOT DELETE
+
+// const REQS_URL = "http://localhost:3000/api/v1/requests"
+// const MODULES = ["Git & GitHub", "Intro to Ruby", "HTML & CSS", "Procedural Ruby", "Object-Oriented Ruby", "SQL", "ORMs & ActiveRecord", "Rack", "Sinatra", "Rails", "JavaScript", "React", "Redux", "Other"]
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log("loaded")
+//   fetchRequests()
+// });
+
+// function fetchRequests() {
+//   fetch(REQS_URL)
+//     .then(resp => resp.json())
+//     .then(requests => console.log("fetchRequests -> requests", requests))
+// }
