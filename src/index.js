8f26393d307584e6ea975fdf9dfedfc9adc7d91f
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-24 20:46:50 -0400

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap';
+
 const REQS_URL = "http://localhost:3000/api/v1/requests"
 const MODULES = ["Git & GitHub", "Intro to Ruby", "HTML & CSS", "Procedural Ruby", "Object-Oriented Ruby", "SQL", "ORMs & ActiveRecord", "Rack", "Sinatra", "Rails", "JavaScript", "React", "Redux", "Other"]
 
