aa8175abd11bcd6bb6243e3c3f723f06508a2415
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-24 23:00:00 -0400

    Install webpacker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+import bootstrap from 'bootstrap';
 
 const REQS_URL = "http://localhost:3000/api/v1/requests"
 const MODULES = ["Git & GitHub", "Intro to Ruby", "HTML & CSS", "Procedural Ruby", "Object-Oriented Ruby", "SQL", "ORMs & ActiveRecord", "Rack", "Sinatra", "Rails", "JavaScript", "React", "Redux", "Other"]
@@ -11,8 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
 function fetchRequests() {
   fetch(REQS_URL)
     .then(resp => resp.json())
-    .then(requests => requests.forEach(requestData => { 
-      newRequest = Request.new(requestData);
-      renderRequest(newRequest);
-    }))
+    .then(requests => console.log("fetchRequests -> requests", requests))
 }
