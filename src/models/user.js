06d52630ca63813e40dc21b1c06b3387dbe3a87a
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-08 11:40:00 -0400

    Build out user class

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(userData) {
+    this.id = userData.id;
+    this.firstName = userData.first_name;
+    this.lastName = userData.last_name;
+    this.emailAddress = userData.email_address;
+    this.role = userData.role;
+    User.call.push(this);
+  }
+
+  static findUser(id) {
+    return this.all.find((user) => user.id === id)
+  }
+
+  static updateUser(userData) {
+    const user = this.findUser(userData.id);
+    user.firstName = userData.first_name;
+    user.lastName = userData.last_name;
+    user.emailAddress = userData.email_address;
+    user.role = userData.role;
+    return user;
+  }
+}
+
+User.all = [];
+
+export { User };
