4d86a1010db84d358a04f30197e64fb74babe219
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-19 21:41:16 -0400

    Add webpack configs and node packages

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = {
+  entry: './src/entry.js',
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sourceMap: true
+            }
+          }
+        ],
+      }, {
+        test: /\.(png|jpg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+        }
+      }, {
+        test: /\.(ttf|svg|otf|eot|woff(2)?)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      }, {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ManifestPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Flatiron Study Group Central',
+      template: 'template.html',
+      scriptLoading: 'defer',
+      meta: {
+        charset: {charset: 'UTF-8'},
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      }
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, '../', 'dist'),
+    publicPath: '/',
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  }
+};
