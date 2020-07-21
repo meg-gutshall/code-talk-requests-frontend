53ddda6979c4c6cc3cd05d315a972be03ed61b5f
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-20 22:28:08 -0400

    Create login form skeleton

--- src/services/domElements.js
+++ src/services/domElements.js
@@ -6,11 +6,11 @@ class DOMElements {
   // const MODS = ["Git & GitHub", "Intro to Ruby", "HTML & CSS", "Procedural Ruby", "Object-Oriented Ruby", "SQL", "ORMs & ActiveRecord", "Rack", "Sinatra", "Rails", "JavaScript", "React", "Redux", "Other"]
 
   static get navMenu() { return document.getElementById('navbarToggler') }
-  static get mainBody() { return document.getElementById('container') }
+  static get mainBody() { return document.getElementById('main-container') }
   static get footerBody() { return document.getElementById('footer-body') }
 
   static createLoginFormContainer() {
-    this.mainBody.innerHTML += `<div class="row row-cols-1" id="login-form-container"></div>`;
+    this.mainBody.innerHTML += `<div class="row"><div class="col" id="login-form-container"></div></div>`;
   }
 
   static get loginFormContainer() {
@@ -19,21 +19,51 @@ class DOMElements {
   }
 
   static createLoginForm() {
-    const loginForm = `<form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    const loginForm = 
+      `<form class="mt-5">
+        <div class="form-row justify-content-center">
+          <div class="col-9 text-center mb-2">
+            <h1>Welcome back to Flatiron School's online learning platform!</h1>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
+        <div class="form-row justify-content-center">
+          <div class="col-9 text-center mb-4">
+            <p>Please sign into your account to continue learning.</p>
+          </div>
         </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="form-row justify-content-center">
+          <div class="col-6">
+            <label for="user-email">Email Address</label>
+          </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-row justify-content-center">
+          <div class="form-group col-6 mb-2">
+            <input type="email" class="form-control" name="email" id="user-email" required="required" aria-describedby="emailHelp">
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="col-4 order-last">
+            <small id="emailHelp" class="form-text text-muted text-right">Forgot password?</small>
+          </div>
+          <div class="col-2 order first">
+            <label for="user-password">Password</label>
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="col-6">
+            <input type="password" class="form-control" name="user-password" id="password" required="required">
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="col-6">
+            <button type="submit" class="btn btn-primary btn-block mt-5" name="commit">Sign in</button>
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="col-6">
+            <a class="btn btn-outline-primary btn-block mt-2" role="button" href="https://github.com/">Sign in with Github</a>
+          </div>
+        </div>
       </form>`;
     this.loginFormContainer.innerHTML = loginForm;
   }
@@ -42,7 +72,7 @@ class DOMElements {
     return this.createLoginForm();
   }
 
-  static createTopicRequestIndex() {
+  static createTopicRequestIndexContainer() {
     this.mainBody.innerHTML += `<div class="row row-cols-1 row-cols-lg-3" id="topic-request-index"></div>`;
   }
   
