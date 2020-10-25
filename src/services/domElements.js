a3ad0dc9ad053db648b8daa2b445217129efc774
Meg Gutshall <meghan.gutshall@gmail.com>
2020-10-24 22:11:14 -0400

    Create modal form template

--- src/services/domElements.js
+++ src/services/domElements.js
@@ -98,6 +98,46 @@ class DOMElements {
 
   // ---------> End topic request row elements
 
+
+  // ---------> Start new topic request form
+
+  static createTopicRequestModal() {
+    const topicRequestModal = 
+      `<div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="newTopicRequestModalLabel">Create a New Topic Request</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="topicRequestSubject">Topic Request Subject</label>
+              <input type="text" class="form-control" id="topicRequestSubject" required>
+            </div>
+            <div class="form-group">
+              <label for="topicRequestDescription">Topic Request Description</label>
+              <textarea class="form-control" id="topicRequestDescription" rows="3"></textarea>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </div>`
+    return topicRequestModal;
+  }
+
+  static get topicRequestModal() {
+    this.createTopicRequestModal();
+    return document.getElementById('newTopicRequestModal');
+    // TODO: Attach to event listener for when navbar button is clicked
+  }
+
+  // ---------> End new topic request form
+
 }
 
 export { DOMElements };
