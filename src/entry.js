21b631f12893562d4d6d7a9f43e479098ad6dc72
Meg Gutshall <meghan.gutshall@gmail.com>
2020-11-07 16:16:59 -0500

    Extract login functionality from the main JS file

--- src/entry.js
+++ src/entry.js
@@ -13,54 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   autoRedirect();
 });
 
-// Show the login form
-function showLoginForm() {
-  DOMElements.loginForm;
-  let loginForm = document.getElementById('login-form');
-  loginForm.addEventListener('submit', e => loginFormHandler(e));
-}
-
-// Transfer the form data on submittal
-async function loginFormHandler(e) {
-  e.preventDefault();
-  const emailInput = e.target.querySelector('#user-email').value;
-  const passwordInput = e.target.querySelector('#user-password').value;
-  await login(emailInput, passwordInput);
-  autoRedirect();
-  // Check here for CORRECT login credentials by calling autoRedirect again
-}
-
-// Auth help article
-// https://zellwk.com/blog/frontend-login-system/
-
-// Login
-async function login(email_address, password) {
-  const newLoginSubmission = {
-    method: 'POST',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify({ user: { email_address, password }})
-  };
-  const loginResponse = await fetch(LOGIN_URL, newLoginSubmission);
-  const userData = await loginResponse.json();
-  localStorage.setItem('jwt_token', userData.jwt);
-  localStorage.setItem('current_user', userData.user.data.id);
-  DOMElements.mainBody.innerHTML = "";
-}
-
-// Check if user is logged in
-async function isLoggedIn() {
-  let token = localStorage.getItem('jwt_token');
-  if (!token) {
-    return false
-  } else {
-    return true
-  }
-}
-
 // Redirect user depending on login status
 async function autoRedirect() {
   const validLogin = await isLoggedIn();
@@ -70,12 +22,6 @@ async function autoRedirect() {
   } else {
     showLoginForm();
   }
-}
-
-// Logout
-function logoutAction() {
-  localStorage.removeItem('jwt_token');
-  localStorage.removeItem('current_user');
 }
 
 // Display topic requests
---
+++ src/services/authorization.js
@@ -0,0 +1,58 @@
+import DOMElements from './domElements';
+import { LOGIN_URL, autoRedirect } from '../entry';
+
+// Show the login form
+function showLoginForm() {
+  DOMElements.loginForm;
+  let loginForm = document.getElementById('login-form');
+  loginForm.addEventListener('submit', e => loginFormHandler(e));
+}
+
+// Transfer the form data on submittal
+async function loginFormHandler(e) {
+  e.preventDefault();
+  const emailInput = e.target.querySelector('#user-email').value;
+  const passwordInput = e.target.querySelector('#user-password').value;
+  await login(emailInput, passwordInput);
+  autoRedirect();
+  // Check here for CORRECT login credentials by calling autoRedirect again
+}
+
+// Auth help article
+// https://zellwk.com/blog/frontend-login-system/
+
+// Login
+async function login(email_address, password) {
+  const newLoginSubmission = {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({ user: { email_address, password }})
+  };
+  const loginResponse = await fetch(LOGIN_URL, newLoginSubmission);
+  const userData = await loginResponse.json();
+  localStorage.setItem('jwt_token', userData.jwt);
+  localStorage.setItem('current_user', userData.user.data.id);
+  DOMElements.mainBody.innerHTML = "";
+}
+
+// Check if user is logged in
+async function isLoggedIn() {
+  let token = localStorage.getItem('jwt_token');
+  if (!token) {
+    return false
+  } else {
+    return true
+  }
+}
+
+// Logout
+// function logoutAction() {
+//   localStorage.removeItem('jwt_token');
+//   localStorage.removeItem('current_user');
+// }
+
+export { isLoggedIn, showLoginForm };
